7676ca328ec6bfcc2d84ecf8831ca6975c91fb77
manhhoach <manhhn.nv5@gmail.com>
2026-05-01 02:12:41 +0100

    update banner

--- components/new/banner.js
+++ components/new/banner.js
@@ -4,24 +4,26 @@ import { francois } from "@/app/layout";
 export default function Banner() {
   return (
     <div
-      className="container mx-auto w-11/12 rounded-xl p-4 py-4 md:w-1/2 md:px-8"
+      className="container mx-auto w-11/12 rounded-xl p-4 py-4 lg:max-w-screen-lg"
       style={{
         backgroundImage: "url('/img/bgbanner.jpg')",
         backgroundSize: "cover"
       }}>
-      <p
+      {/* <p
         className={`text-2xl font-bold text-[#223e76] md:text-3xl ${francois.className}`}>
         FinPoint - Finance for Freedom
-      </p>
+      </p> */}
       <p
         className={`pt-2 text-sm text-gray-700 md:w-1/2 md:text-base`}>
-        Giải thích tài chính, doanh nghiệp và tiền bạc cá nhân theo
-        cách dễ hiểu, thực tế và thú vị
+        Chúng tôi xây dựng <span className="font-bold text-[#223e76]">FinPoint</span> nhằm đem đến cho bạn
+        những kiến thức trong lĩnh vực Tài chính - Chứng khoán, về góc
+        nhìn mới về doanh nghiệp và kĩ năng quản lí tài chính cá nhân
+        cho bạn
       </p>
       <Link
         href="/archive"
         className={`mt-4 inline-block rounded-md bg-[#223e76] px-5 py-2 text-sm font-medium text-white hover:bg-[#223e76]/90`}>
-        Khám phá bài viết
+        Xem bài viết
       </Link>
     </div>
   );
--- components/new/categoryIcon.js
+++ components/new/categoryIcon.js
@@ -24,10 +24,10 @@ export default function CategoryIcon({ category, isActive }) {
   }
   return (
     <div
-      className={`relative w-1/3  cursor-pointer rounded-lg border-[2px] px-4 py-2 `}
+      className={`relative w-1/3 cursor-pointer rounded-lg border px-4 py-2`}
       style={{
-        borderColor: color,
-        backgroundColor: hover ? color + "10" : color + "25"
+        borderColor: hover ? color : "",
+        backgroundColor: hover ? color + "25" : color + "10"
       }}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
