d3f2476c9c0eaeb9a7e9380906a72108a6d86a9c
manhhoach <manhhn.nv5@gmail.com>
2026-05-02 04:48:44 +0100

    .

--- components/footer.js
+++ components/footer.js
@@ -7,14 +7,15 @@ export default function Footer(props) {
       <div className="text-center text-sm">
         Copyright © {new Date().getFullYear()} {props?.copyright}. All
         rights reserved.
+        <p className="mt-4 text-sm text-gray-700">
+          Các quan điểm được chia sẻ trên FinPoint là quan điểm cá
+          nhân, không đại diện cho tổ chức nơi chủ sở hữu đang công
+          tác.
+        </p>
       </div>
-      <div className="mt-2 flex items-center justify-between">
-        <div className="mt-5">
-
-        </div>
+      <div className="mt-2 flex items-center justify-end">
         <ThemeSwitch />
       </div>
     </Container>
   );
 }
-
