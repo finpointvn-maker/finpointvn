d3ecd2d08ffdbfefd167007c0c41496b3da4976a
manhhoach <manhhn.nv5@gmail.com>
2026-05-09 10:02:54 +0100

    fix redirect link

--- components/blog/authorCard.js
+++ components/blog/authorCard.js
@@ -10,7 +10,7 @@ export default function AuthorCard({ author }) {
       <div className="flex flex-wrap items-start sm:flex-nowrap sm:space-x-6">
         <div className="relative mt-1 h-24 w-24 flex-shrink-0 ">
           {imageProps && (
-            <Link href={`/author/${author.slug.current}`}>
+            <Link href={`/about`}>
               <Image
                 src={imageProps.src}
                 alt={author.name}
@@ -32,7 +32,7 @@ export default function AuthorCard({ author }) {
           </div>
           <div className="mt-3">
             <Link
-              href={`/author/${author.slug.current}`}
+              href={`/about`}
               className="bg-brand-secondary/20 rounded-full py-2 text-sm text-blue-600 dark:text-blue-500 ">
               View Profile
             </Link>
